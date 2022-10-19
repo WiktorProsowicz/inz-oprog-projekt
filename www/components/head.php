d0dffbdb6dc028f402aff20c46b5b981f9c0a388
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2022-10-19 18:28:09 +0200

    main page header, categories

---
+++ www/components/head.php
@@ -0,0 +1,26 @@
+<div class="w-100">
+    <div class="head d-flex justify-content-between align-items-center">
+
+        <a class="head__logoHolder" href="/">
+            <img src="/media/logo.png" alt="site's logo"/>
+        </a>
+
+        <div class="head__search">
+            <form action="/search.php" method="get" class="d-flex align-items-center">
+                <input type="text" placeholder="wpisz tag, kategorię" name="q" class="head__searchInput"/>
+                <input type="submit" value="Wyszukaj" class="head__searchBtn"/>
+            </form>
+        </div>
+
+        <div class="head__profile">
+
+            <a href="#" class="d-flex align-items-center justify-content-center link-secondary">
+                <img class="head__profileImg" src="/media/user_profile_template.png" alt="user profile img"/>
+
+                <span class="head__profileName">zaloguj się</span>
+            </a>
+
+        </div>
+
+    </div>
+</div>
--- www/index.php
+++ www/index.php
@@ -1,12 +1,78 @@
 <!Doctype html>
-<html>
+<html lang="pl">
 
     <head>
 
+        <meta name="charset" content="utf-8">
+        <meta name="author" content="Wiktor Prosowicz" />
+        <meta name="description" content="Forum internetowe, gdzie możesz publikować wiersze, dowcipy i jakąkolwiek inną treść tekstową :)">
+        <meta name="keywords" content="forum, poems, wiersze, publikuj, publikowanie" />
+        <meta http-equiv="x-ua-compatibile" content="chrome=1,ie=edge" />
+        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+
+        <title>Forum ziomeczków - strona główna</title>
+
+        <!-- offline downloaded jquery files for developement-->
+        <script src="/jquery/jquery-3.6.1.min.js"></script>
         
+        <!-- offline downloaded bootstrap files for developement -->
+        <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- custom stylesheets -->
+        <style>
+            a {
+                text-decoration: none !important;
+            }
+        </style>
+        <link href="/style/head.css" rel="stylesheet"/>
+        <link href="/style/index.css" rel="stylesheet"/>
 
     </head>
 
-    <body></body>
+    <body>
+
+        <?php 
+            require "./components/head.php";
+        ?>
+
+        <div class="main d-flex">
+
+            <div class="main__categories">
+
+                <ul class="main__categoriesList">
+
+                    <?php 
+
+                        // hard-coded categories array
+                        $categories = array(
+                            "śmieszne", "wiersze", "wiadomości", "zainteresowania", "lifehacki", "opowiadania", "gore", "opowiadania erotyczne"
+                        );
+
+                        foreach ($categories as $cat) {
+                            echo '
+                                <li>
+                                    <a href="/search.php?c='.$cat.'">'
+                                        .$cat.
+                                    '</a>
+                                </li>
+                            ';
+                        }
+
+                    ?>
+
+                </ul>
+
+            </div>
+
+
+            <div class="main__grid">
+
+
+            </div>
+
+        </div>
+
+    </body>
 
 </html>
