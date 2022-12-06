9febb2f7c4d9aa3cf1540f144399dbb443c9f94e
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2022-12-07 00:26:26 +0100

    comments reports

--- www/notifications_bound_scripts.php
+++ www/notifications_bound_scripts.php
@@ -63,11 +63,15 @@
 
             if($result->fetch_array()[0] < 24) {
 
+                $result->free_result();
+
                 $_SESSION["read_reportpostmsg"] = "Możesz ponownie zgłosić ten post dopiero po 24 godzinach.";
 
                 header("Location: read.php?p=" . $reportedId);
                 exit();
             }
+
+            $result->free_result();
 
         }
 
@@ -83,12 +87,13 @@
             $content = 'Użytkownik <a href="profile.php?u='.$_SESSION["user_username"].'">'.
                         $_SESSION["user_username"].
                     '</a>
-                    zgłosił post <a href="read.php?p='.$reportedId.'"></a>.';
+                    zgłosił post <a href="read.php?p='.$reportedId.'">'.$reportedId.'</a>.
+                    "'.$reportedContent.'"';
             
             // inseritng a notification with message to admin
             $query = sprintf("INSERT INTO notifications
-                            (`author_id`, `recipient_id`, `content`, `high_priority`, `watched`)
-                            VALUES (%d, %d, '%s', true, false);",
+                            (`author_id`, `recipient_id`, `content`, `high_priority`, `watched`, `date`)
+                            VALUES (%d, %d, '%s', true, false, NOW());",
                             $_SESSION["user_id"], $leastOccupiedAdminId, $connection->real_escape_string($content));
            
             $connection->query($query);
@@ -97,6 +102,68 @@
         
         header("Location: read.php?p=" . $reportedId);
         exit();
+    }
+
+
+    // reporting a comment
+    if(isset($_POST["commentReport"])) {
+
+        $reportedId = $_POST["commentReportId"];
+        $reportedContent = $_POST["commentReportContent"];
+
+        // getting interval since last report of that comment from this user
+        $query = sprintf("SELECT COALESCE(EXTRACT(HOUR FROM NOW() - r.date), 0)
+                    FROM `reports` r
+                    WHERE r.post_id <=> %d AND r.author_id = %d
+                    ORDER BY 1 ASC LIMIT 1;", $reportedId, $_SESSION["user_id"]);
+
+        $result = $connection->query($query);
+
+        if($result->num_rows > 0) {
+
+            if($result->fetch_array()[0] < 24) {
+                $result->free_result();
+                $_SESSION["commentreportmsg"] = array($reportedId, "Możesz ponownie zgłosić ten komentarz dopiero po 24 godzinach.");
+
+                header("Location: read.php?p=" . $_SESSION["read_currentViewedPost"] . "#" . $reportedId);
+                exit();
+
+            }
+
+            $result->free_result();
+
+        }
+
+
+        // inserting a report to database
+        $query = sprintf("INSERT INTO reports 
+                        (`author_id`, `comment_id`, `content`, `date`) VALUES (%d, %d, '%s', NOW());",
+                        $_SESSION["user_id"], $reportedId, $reportedContent);
+        $connection->query($query);
+
+        $leastOccupiedAdminId = get_least_occupied_admin($connection);
+
+        if($leastOccupiedAdminId !== null) {
+            $content = 'Użytkownik <a href="profile.php?u='.$_SESSION["user_username"].'">'.
+                        $_SESSION["user_username"].
+                    '</a>
+                    zgłosił komentarz <a href="read.php?p='.$_SESSION["read_currentViewedPost"].'#'.$reportedId.'">'.$reportedId.'</a>.
+                    "'.$reportedContent.'"';
+            
+            // inseritng a notification with message to admin
+            $query = sprintf("INSERT INTO notifications
+                            (`author_id`, `recipient_id`, `content`, `high_priority`, `watched`, `date`)
+                            VALUES (%d, %d, '%s', true, false, NOW());",
+                            $_SESSION["user_id"], $leastOccupiedAdminId, $connection->real_escape_string($content));
+           
+            $connection->query($query);
+        }
+        
+        
+        header("Location: read.php?p=" . $_SESSION["read_currentViewedPost"] . "#" . $reportedId);
+        exit();
+
+
     }
 
 ?>
